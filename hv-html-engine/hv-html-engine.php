9020d7267a5631c5f00987bb3154ef8953c30104
Dr-PPSN <kairohrpasser@gmail.com>
2023-01-22 20:40:07 +0100

    + some new classes

--- hv-html-engine/hv-html-engine.php
+++ hv-html-engine/hv-html-engine.php
@@ -1,6 +1,7 @@
 <?php
 require_once("hv-table.php");
 require_once("hv-a.php");
+require_once("hv-p.php");
 
 class HV_HTML_Engine{
     public function getTable($array2D, $class, $id, $style, $orderBy, $orderDirection, $thClass, $tdClass){
@@ -12,6 +13,10 @@ class HV_HTML_Engine{
     public function getA($text, $class, $id, $style, $href){
         $a = new HV_A($text, $class, $id, $style, $href);
         return $a->getA();
+    }
+    public function getP($text, $class, $id, $style, $src){
+        $p = new HV_P($text, $class, $id, $style, $src, "", "");
+        return $p->getP();
     }
 }
 
