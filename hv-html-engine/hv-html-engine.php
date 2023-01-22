13a43357b7811c6ed3271b22f30d49b1050444ae
Dr-PPSN <kairohrpasser@gmail.com>
2023-01-22 15:16:52 +0100

    + engine can now generate a tags and tables

--- hv-html-engine/hv-html-engine.php
+++ hv-html-engine/hv-html-engine.php
@@ -1,8 +1,19 @@
 <?php
+require_once("hv-table.php");
+require_once("hv-a.php");
 
-include "table.php";
-
-echo "HV-HTML-Engine Start";
-
+class HV_HTML_Engine{
+    public function __construct(){
+        echo "HV-HTML-Engine Start<br>";
+    }
+    public function getTable($array2D, $class, $id, $style, $orderBy, $orderDirection){
+        $table = new HV_Table($array2D, $class, $id, $style, $orderBy, $orderDirection);
+        return $table->getTable();
+    }
+    public function getA($text, $class, $id, $style, $href){
+        $a = new HV_A($text, $class, $id, $style, $href);
+        return $a->getA();
+    }
+}
 
 ?>
