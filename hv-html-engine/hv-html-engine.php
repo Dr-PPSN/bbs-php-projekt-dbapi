5454b08dc81567f3b0e50ee136abd6c0c451bab1
Dr-PPSN <kairohrpasser@gmail.com>
2023-01-22 15:59:14 +0100

    + added th class and td class property

--- hv-html-engine/hv-html-engine.php
+++ hv-html-engine/hv-html-engine.php
@@ -3,11 +3,10 @@ require_once("hv-table.php");
 require_once("hv-a.php");
 
 class HV_HTML_Engine{
-    public function __construct(){
-        echo "HV-HTML-Engine Start<br>";
-    }
-    public function getTable($array2D, $class, $id, $style, $orderBy, $orderDirection){
+    public function getTable($array2D, $class, $id, $style, $orderBy, $orderDirection, $thClass, $tdClass){
         $table = new HV_Table($array2D, $class, $id, $style, $orderBy, $orderDirection);
+        $table->setThClass($thClass);
+        $table->setTdClass($tdClass);
         return $table->getTable();
     }
     public function getA($text, $class, $id, $style, $href){
--- hv-html-engine/hv-table.php
+++ hv-html-engine/hv-table.php
@@ -2,13 +2,51 @@
 require_once("hv-html.php");
 
 class HV_Table extends HV_HTML{
+    public $thClass = "";
+    public $tdClass = "";
     public function __construct($array2D, $class, $id, $style, $orderBy, $orderDirection)
     {
         parent::__construct("", $array2D, $class, $id, $style, "", $orderBy, $orderDirection);
     }
+    public function setThClass($class){
+        $this->thClass = $class;
+    }
+    public function setTdClass($tdClass){
+        $this->tdClass = $tdClass;
+    }
     public function getTable()
     {
-        return "returned table";
+        $table = "<table";
+        if($this->class != ""){
+            $table .= " class='$this->class'";
+        }
+        if($this->id != ""){
+            $table .= " id='$this->id'";
+        }
+        if($this->style != ""){
+            $table .= " style='$this->style'";
+        }
+        $table .= ">";
+        foreach($this->array2D as $key => $value){
+            if($key == 0){
+                $table .= "<tr>";
+                foreach($value as $key2 => $value2){
+                    $table .= "<th";
+                    if($this->thClass != ""){
+                        $table .= " class='$this->thClass'";
+                    }
+                    $table .= ">$key2</th>";
+                }
+                $table .= "</tr>";
+            }
+            $table .= "<tr>";
+            foreach($value as $key2 => $value2){
+                $table .= "<td class='$this->tdClass'>$value2</td>";
+            }
+            $table .= "</tr>";
+        }
+        $table .= "</table>";
+        return $table;
     }
 }
 ?>
