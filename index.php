2cc6dd0e1286b1f9084939387395c5dfcd9406b7
DESKTOP-FOASM6G\User <dennis.schneeweiss@web.de>
2023-03-06 11:36:43 +0100

    parallax effect usable

--- index.php
+++ index.php
@@ -1,14 +1,22 @@
 <!DOCTYPE html>
-<html lang="de">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BBS Projekt BahnAPI</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="stylesheet.css">
 </head>
 <body>
-    <?php
-        echo "Hallo Welt";
-    ?>
+
+<p>Scroll Up and Down this page to see the parallax scrolling effect.</p>
+
+<div class="parallax" Style="background-image: url('essets/sweetKitten.jpg');"></div>
+
+<div style="height:1000px;background-color:red;font-size:36px">
+Scroll Up and Down this page to see the parallax scrolling effect.
+This div is just here to enable scrolling.
+Tip: Try to remove the background-attachment property to remove the scrolling effect.
+</div>
+
+<div class="parallax" Style="background-image: url('essets/sweetKitten2.jpg');"></div>
+
 </body>
 </html>
