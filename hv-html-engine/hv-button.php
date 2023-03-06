aa30eb06f23593ab4d97ebe45eca7d3d79458c61
Kai Rohrpasser <kairohrpasser@gmail.com>
2023-03-06 12:23:56 +0100

    + create button and img

--- hv-html-engine/hv-button.php
+++ hv-html-engine/hv-button.php
@@ -7,19 +7,19 @@ class HV_Button extends HV_HTML
     {
         parent::__construct($text, "", $class, $id, $style, "", "", "");
     }
-    public function getP()
+    public function getButton()
     {
-        $p = "<p";
-        if($this->class != ""){
-            $p .= " class='$this->class'";
+        $button = "<button";
+        if ($this->class != "") {
+            $button .= " class='" . $this->class . "'";
         }
-        if($this->id != ""){
-            $p .= " id='$this->id'";
+        if ($this->id != "") {
+            $button .= " id='" . $this->id . "'";
         }
-        if($this->style != ""){
-            $p .= " style='$this->style'";
+        if ($this->style != "") {
+            $button .= " style='" . $this->style . "'";
         }
-        $p .= ">$this->text</p>";
-        return $p;
+        $button .= ">" . $this->text . "</button>";
+        return $button;
     }
 }
--- hv-html-engine/hv-html-engine.php
+++ hv-html-engine/hv-html-engine.php
@@ -2,6 +2,8 @@
 require_once("hv-table.php");
 require_once("hv-a.php");
 require_once("hv-p.php");
+require_once("hv-button.php");
+require_once("hv-img.php");
 
 class HV_HTML_Engine{
     public function getTable($array2D, $class, $id, $style, $orderBy, $orderDirection, $thClass, $tdClass){
@@ -17,6 +19,14 @@ class HV_HTML_Engine{
     public function getP($text, $class, $id, $style){
         $p = new HV_P($text, $class, $id, $style);
         return $p->getP();
+    }
+    public function getButton($text, $class, $id, $style){
+        $button = new HV_Button($text, $class, $id, $style);
+        return $button->getButton();
+    }
+    public function getImg($class, $id, $style, $src){
+        $img = new HV_Img($class, $id, $style, $src);
+        return $img->getImg();
     }
 }
 
