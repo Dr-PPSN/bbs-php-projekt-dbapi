308261cc0827d8f352a2fb4894be82added7a3be
Robert <robert.goepfert1999@gmail.com>
2023-03-08 13:26:07 +0100

    ~ Formatierung

--- Api/api-service.php
+++ Api/api-service.php
@@ -3,17 +3,35 @@
 require_once 'getData.php';
 
 function getStationData(int $stationID): array|string {
-  return getData("https://apis.deutschebahn.com/db-api-marketplace/apis/station-data/v2/stations/" . $stationID);
+  $data = getData("https://apis.deutschebahn.com/db-api-marketplace/apis/station-data/v2/stations/" . $stationID);
+  if (false === $data) {
+    return false;
+  } else {
+    // TODO: Pick relevant data from $data
+    return $data;
+  }
 }
 
 
 function getStationPictureURL(int $stationID): array|string {
-  return getData("https://apis.deutschebahn.com/db-api-marketplace/apis/api.railway-stations.org/photoStationById/de/" . $stationID);
+  $data = getData("https://apis.deutschebahn.com/db-api-marketplace/apis/api.railway-stations.org/photoStationById/de/" . $stationID);
+  if (false === $data) {
+    return false;
+  } else {
+    // TODO: Pick relevant data from $data
+    return $data;
+  }
 }
 
 
 function getFahrplan(int $evaNr, string $date, string $hour): array|string {
-  return getData("https://apis.deutschebahn.com/db-api-marketplace/apis/timetables/v1/plan/" . $evaNr . "/" . $date . "/" . $hour, 'xml');
+  $data = getData("https://apis.deutschebahn.com/db-api-marketplace/apis/timetables/v1/plan/" . $evaNr . "/" . $date . "/" . $hour, 'xml');
+  if (false === $data) {
+    return false;
+  } else {
+    // TODO: Pick relevant data from $data
+    return $data;
+  }
 }
 
 
