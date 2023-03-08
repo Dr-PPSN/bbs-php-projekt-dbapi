d1143fd56e5a91cafa98809e96286ce1182edb59
Robert <robert.goepfert1999@gmail.com>
2023-03-08 13:26:07 +0100

    ~ Formatierung

--- Api/api-service.php
+++ Api/api-service.php
@@ -3,22 +3,40 @@
 require_once 'getData.php';
 
 function getStationData(int $stationID): array|string {
-  return getData("https://apis.deutschebahn.com/db-api-marketplace/apis/station-data/v2/stations/" . $stationID);
+  $data = getData("https://apis.deutschebahn.com/db-api-marketplace/apis/station-data/v2/stations/" . $stationID);
+  if (false === $data) {
+    return false;
+  } else {
+    // TODO: Pick relevant data from $data
+    return $data;
+  }
 }
 
 
 function getStationPictureURL(int $stationID): array|string {
-  return getData("https://apis.deutschebahn.com/db-api-marketplace/apis/api.railway-stations.org/photoStationById/de/" . $stationID);
+  $data = getData("https://apis.deutschebahn.com/db-api-marketplace/apis/api.railway-stations.org/photoStationById/de/" . $stationID);
+  if (false === $data) {
+    return false;
+  } else {
+    // TODO: Pick relevant data from $data
+    return $data;
+  }
 }
 
 
 function getFahrplan(int $evaNr, string $date, string $hour): array|string {
-  return getData("https://apis.deutschebahn.com/db-api-marketplace/apis/timetables/v1/plan/" . $evaNr . "/" . $date . "/" . $hour, 'xml');
+  $data = getData("https://apis.deutschebahn.com/db-api-marketplace/apis/timetables/v1/plan/" . $evaNr . "/" . $date . "/" . $hour, 'xml');
+  if (false === $data) {
+    return false;
+  } else {
+    // TODO: Pick relevant data from $data
+    return $data;
+  }
 }
 
 
 var_dump(getStationData(3881));
-echo "<br><br>";
+echo "<br><br>";  // linebreak go br
 var_dump(getStationPictureURL(3881));
 echo "<br><br>";
 var_dump(getFahrplan(8010224, 230308, 14));
--- Api/getData.php
+++ Api/getData.php
@@ -5,16 +5,16 @@ require_once '../config.php';
 
 function getResponseType(string $responseType): string {
     switch ($responseType) {
-        case 'json':
-            return 'application/json';
-        case 'xml':
-            return 'application/xml';
-        default:
-            return 'application/json';
+      case 'json':
+        return 'application/json';
+      case 'xml':
+        return 'application/xml';
+      default:
+        return 'application/json';
     }
 }
 
-function getData(string $url, string $responseType = 'json'): string|array {
+function getData(string $url, string $responseType = 'json'): bool|string|array {
   $cw = new Curl\CurlWrapper($url);
   $cw->setHeaders(
     array(
@@ -22,12 +22,12 @@ function getData(string $url, string $responseType = 'json'): string|array {
       'DB-Client-Id: ' . CLIENT_ID,
       'DB-Api-Key: ' . API_KEY
     ));
-    
+
   $response = $cw->request(new Curl\Requests\GetRequest(array()));
   $cw->dispose();
 
   if (200 !== $response->getStatusCode()) {
-    return "Error: " . $response->getStatusCode();
+    return false;
   } else {
     return $response->decode();
   }
