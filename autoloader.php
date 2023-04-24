d96506f1d727c9aceb5e6014fc3af7f020bb0250
Robert <robert.goepfert1999@gmail.com>
2023-04-24 21:43:06 +0200

    ~ FIX: Autoloader

--- autoloader.php
+++ autoloader.php
@@ -4,11 +4,8 @@ final class Autoloader
     public static function register(): void
     {
         spl_autoload_register(function ($class) {
-            echo $class . "<br>";
-            $file = __DIR__ . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
-            echo $file;
+            $file = __DIR__ . DIRECTORY_SEPARATOR . 'Api' . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
             if (file_exists($file)) {
-                echo "file exists";
                 require $file;
                 return true;
             }
@@ -17,7 +14,6 @@ final class Autoloader
     }
 }
 
-echo __DIR__ . '<br>';
 Autoloader::register();
 
 ?>
