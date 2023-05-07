eee60d334e379959395c3aec3fd544ec8cc5726f
Robert Göpfert <robert.goepfert1999@gmail.com>
2023-05-07 21:55:08 +0200

    ~ config

--- Api/api-config.php
+++ Api/api-config.php
@@ -1,6 +1,6 @@
 <?php
 
-$config = parse_ini_file('../config.ini'); 
+$config = parse_ini_file('api-config.ini'); 
 
 define('CLIENT_ID', $config["client_id"]);
 define('API_KEY'  , $config["api_key"]);
